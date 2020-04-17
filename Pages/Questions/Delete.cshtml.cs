1bd614bdff173344a1d5b8e2d754559b10b05fb3
Douglas Starnes <douglas.a.starnes@gmail.com>
2020-04-17 18:29:29 -0500

    Cleanup UI for auth

--- Pages/Questions/Delete.cshtml.cs
+++ Pages/Questions/Delete.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Quanda.Areas.Identity.Data;
@@ -12,13 +13,16 @@ namespace Quanda.Pages.Questions
     public class DeleteModel : PageModel
     {
         private readonly QuandaIdentityDbContext context;
+        private readonly UserManager<Author> userManager;
+
         public Question Question { get; set; }
 
-        public DeleteModel(QuandaIdentityDbContext context)
+        public DeleteModel(QuandaIdentityDbContext context, UserManager<Author> userManager)
         {
             this.context = context;
+            this.userManager = userManager;
         }
-        public IActionResult OnGet(int questionId)
+        public async Task<IActionResult> OnGet(int questionId)
         {
             Question = context.Questions.FirstOrDefault(q => q.QuestionId == questionId);
 
@@ -27,12 +31,26 @@ namespace Quanda.Pages.Questions
                 return RedirectToPage("../Shared/ErrorNotFound");
             }
 
+            var author = await userManager.GetUserAsync(User);
+
+            if (author.Id != Question.AuthorId)
+            {
+                return RedirectToPage("../Shared/ErrorNotAuthorized");
+            }
+
             return Page();
         }
 
-        public IActionResult OnPost(int questionId)
+        public async Task<IActionResult> OnPost(int questionId)
         {
             Question = context.Questions.FirstOrDefault(q => q.QuestionId == questionId);
+
+            var author = await userManager.GetUserAsync(User);
+
+            if (author.Id != Question.AuthorId)
+            {
+                return RedirectToPage("../Shared/ErrorNotAuthorized");
+            }
 
             context.Questions.Remove(Question);
             context.SaveChanges();
--- Pages/Questions/Edit.cshtml.cs
+++ Pages/Questions/Edit.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Quanda.Areas.Identity.Data;
@@ -12,16 +13,26 @@ namespace Quanda.Pages.Questions
     public class EditModel : PageModel
     {
         private readonly QuandaIdentityDbContext context;
+        private readonly UserManager<Author> userManager;
+
         [BindProperty]
         public Question Question { get; set; }
 
-        public EditModel(QuandaIdentityDbContext context)
+        public EditModel(QuandaIdentityDbContext context, UserManager<Author> userManager)
         {
             this.context = context;
+            this.userManager = userManager;
         }
-        public IActionResult OnGet(int questionId)
+        public async Task<IActionResult> OnGet(int questionId)
         {
             Question = context.Questions.FirstOrDefault(q => q.QuestionId == questionId);
+
+            var author = await userManager.GetUserAsync(User);
+
+            if (author.Id != Question.AuthorId)
+            {
+                return RedirectToPage("../Shared/ErrorNotAuthorized");
+            }
 
             if (Question == null)
             {
@@ -31,9 +42,16 @@ namespace Quanda.Pages.Questions
             return Page();
         }
 
-        public IActionResult OnPost(int questionId)
+        public async Task<IActionResult> OnPost(int questionId)
         {
             Question.Edited = DateTime.Now;
+
+            var author = await userManager.GetUserAsync(User);
+
+            if (author.Id != Question.AuthorId)
+            {
+                return RedirectToPage("../Shared/ErrorNotAuthorized");
+            }
 
             context.Questions.Update(Question);
             context.SaveChanges();
